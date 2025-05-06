03580598dd5de2ffa6481ace1e79a9fad9f80ed3
Sibory Gueye <sibory.gueye@gmail.com>
2025-05-06 15:08:38 +0200

    updt

--- Semaine-3/lost.cbl
+++ Semaine-3/lost.cbl
@@ -2,26 +2,33 @@
        PROGRAM-ID. lost.
            DATA DIVISION.
        WORKING-STORAGE SECTION.
-       77 F           PIC 9(03) VALUE 0.    
-       77 I     PIC 9(03) VALUE 1.
-       77 B        PIC a9(05) VALUE 0
-       77 O              PIC 9(05) VAL 1
-       77 N                           PIC 9(05).
+       77  WS-MAX-NBR           PIC 9(03) VALUE 0.    
+       77  WS-I     PIC 9(03) VALUE 1.
+       77  WS-B        PIC 9(05) VALUE 0
+       77  WS-O              PIC 9(05) VALUE 1.
+       77  WS-N                           PIC 9(05).
 
        PROCEDURE DIVISION.
+      *    Demande du nombre de valeurs que l'on veut afficher
            DISPLAY "Entrez le nombre de termes attendu : ".
-           ACCEPT F
+           ACCEPT WS-MAX-NBR.
 
-       IF F < 1 THEN
-       DISPLAY "Valeur invalide. Le nombre doit être >= 1."
+           IF F < 1 THEN
+           DISPLAY "Valeur invalide. Le nombre doit être >= 1."
            ELSE
                DISPLAY "<inserer le nom de la suite> :"
                DISPLAY B
 
-       IF F > 1
-        DISPLAY O      PERFORM UNTIL I > (F - 2)
-       COMPUTE N = B + O   DISPLAY N
-       MOVE O TO B  MOVE N TO O  ADD 1 TO I
-       END-IF       END-IF.
+               IF WS-MAX-NBR >= 1
+                   DISPLAY O      
+                   PERFORM UNTIL I > (WS-MAX-NBR - 2)
+                   COMPUTE N = B + O   
+                   DISPLAY N
+                   MOVE O TO B  
+                   MOVE N TO O  
+                   ADD 1 TO I
+                   END-PERFORM
+               END-IF       
+           END-IF.
 
            STOP RUN.
